646af8a5bd6c64bc79b936d8a4ebe0849d6acd28
Marcio-VOT <marciofsantos65@gmail.com>
2023-03-08 01:16:11 -0300

    fix: removendo dependencia problematica

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import GlobalStyleComponent from "./styles/GlobalStyle";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Login/Login";
-import { NavBar } from "./comps/NavBar/NavBar";
+import { HomePage } from "./pages/HomePage/HomePage";
 
 export default () => {
   return (
     <BrowserRouter>
       <GlobalStyleComponent />
-      <NavBar />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/timeline" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { NavBar } from "../../comps/NavBar/NavBar";
+
+export const HomePage = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+};
