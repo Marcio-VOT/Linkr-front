46b20eceb55798668f544e4adb195d0fedb80916
Marcio-VOT <marciofsantos65@gmail.com>
2023-03-10 02:47:49 -0300

    feat: responsividade search ba

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -48,7 +48,8 @@ export default function HomePage() {
 const HomePageContainer = styled.div`
   justify-content: center;
   width: 100%;
-  height: 100%;
+  min-height: 100vh;
+  height: auto;
   display: flex;
   background-color: #333333;
 
